48bf8878c9824144d9ec6372df363c2337b3b779
Fabricyo <bass.barreto@gmail.com>
2022-06-09 07:02:06 -0300

    initial config, add resources, styling initial

---
+++ App.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import imageOn from './assets/icons/eco-light.png';
+import imageOff from './assets/icons/eco-light-off.png';
+import logoDioWhite from './assets/icons/logo-dio.png';
+import logoDio from './assets/icons/logo-dio-white.png';
+
+function App() {
+  const toggle = true;
+
+  return (
+    <View style={toggle ? styles.containerLight : styles.container}>
+      <TouchableOpacity onPress={() => {}}>
+        <Image
+          source={toggle ? imageOn : imageOff}
+          style={toggle ? styles.lightingOn : styles.lightingOff}
+        />
+        <Image
+          source={toggle ? logoDioWhite : logoDio}
+          style={styles.dioLogo}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 200,
+    height: 200,
+  },
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 200,
+    height: 200,
+  },
+  dioLogo: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
+  },
+});
+
+export default App;
