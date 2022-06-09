128493ec00f396df7d44530d2d69ddc05411c548
Fabricyo <bass.barreto@gmail.com>
2022-06-09 07:06:48 -0300

    handleToggle

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 import imageOn from './assets/icons/eco-light.png';
 import imageOff from './assets/icons/eco-light-off.png';
@@ -6,11 +6,15 @@ import logoDioWhite from './assets/icons/logo-dio.png';
 import logoDio from './assets/icons/logo-dio-white.png';
 
 function App() {
-  const toggle = true;
+  const [toggle, setToggle] = useState(false);
+
+  function handleToggle() {
+    setToggle(!toggle);
+  }
 
   return (
     <View style={toggle ? styles.containerLight : styles.container}>
-      <TouchableOpacity onPress={() => {}}>
+      <TouchableOpacity onPress={handleToggle}>
         <Image
           source={toggle ? imageOn : imageOff}
           style={toggle ? styles.lightingOn : styles.lightingOff}
